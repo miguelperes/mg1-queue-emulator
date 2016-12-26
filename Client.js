770bc1705b126ca9a5096ca2c8d42c0c667be074
Miguel Péres <foxmpc@gmail.com>
2016-12-26 19:27:16 -0200

    Adds metrics E[U], E[W], E[B], E[T], E[N]

--- Client.js
+++ Client.js
@@ -3,6 +3,7 @@ const ClientClass = Object.freeze( {A: 0, B: 1} );
 function Client(clientClass, serviceTime) {
 	this.class = clientClass;
 	this.serviceTime = serviceTime;
+	this.residualServiceTime = serviceTime;
 	this.arrivalTime = 0.0;
 
 	this.registerArrival = function(arrivalTime) {
--- Queue.js
+++ Queue.js
@@ -2,6 +2,13 @@ const TypeEnum = Object.freeze( {FCFS: 0, LCFS: 1} );
 const queueDiv = document.getElementById('fila');
 const serverDiv = document.getElementById('server');
 
+const avgSystemTimeDiv     = document.getElementById('avg-system-time');
+const avgWaitTimeDiv       = document.getElementById('avg-wait-time');
+const avgPendingServiceDiv = document.getElementById('avg-pending-service');
+const avgBusyTimeDiv       = document.getElementById('avg-busy-time');
+const avgClientsInQueueDiv = document.getElementById('avg-clients-queue');
+
+
 function QueueSimulator() {
 	this.type = TypeEnum.FCFS;
 	this.isPreemptive = false;
@@ -13,20 +20,19 @@ function QueueSimulator() {
 	this.queueA = [];
 	this.queueB = [];
 
-	this.classAServiceTime = 1/120;
+	this.classAServiceTime = 1/5;
 	this.classBServiceTime = 1/60;
 
-	this.start = function(){
-		this.arrivalsGeneratorA = new ArrivalsGenerator(1/10, ClientClass.A, this.classAServiceTime, this);
-		this.arrivalsGeneratorA.start();
+	this.classAArrivalTime = 1/6;
+	this.classBArrivalTime = 1/60;
 
-		setInterval(this.processClient.bind(this), 0);
-	}
+	this.totalWaitTime = 0.0;
+	this.totalSystemTime = 0.0;
+	this.numOfServedClients = 0;
 
-	this.addNewClient = function(newClient){
-		this.queueA.push(newClient);
-		newClient.registerArrival( Date.now() );
-	}
+	this.totalPendingService = 0.0;
+	this.totalClientsInQueue = 0.0;
+	this.totalArrivals = 0;
 
 	this.currentClient = null;
 
@@ -34,40 +40,103 @@ function QueueSimulator() {
 	this.lastServiceTime = 0.0;
 	this.currentServiceTime = 0.0;
 
+	this.enteredInBusyTime = false;
+	this.busyTimeStartTime = 0.0;
+	this.totalBusyTime = 0.0;
+	this.countOfBusyTimes = 0;
+
+	this.start = function(){
+		this.arrivalsGeneratorA = new ArrivalsGenerator(this.classAArrivalTime, ClientClass.A, this.classAServiceTime, this);
+		this.arrivalsGeneratorA.start();
+
+		setInterval(this.processClient.bind(this), 1);
+	}
+
+	this.addNewClient = function(newClient){
+		
+		this.totalArrivals ++;
+
+		this.totalClientsInQueue += this.queueA.length;
+
+		if(this.isAttending)
+			this.totalPendingService += this.currentClient.residualServiceTime;
+		
+		for(var clientNum in this.queueA) {
+			this.totalPendingService += this.queueA[clientNum].residualServiceTime;
+		}
+
+		if(!this.isAttending && this.queueA.length == 0)
+		{
+			this.busyTimeStartTime = Date.now() / 1000;
+			this.enteredInBusyTime = true;
+			this.countOfBusyTimes ++;
+		}
+
+		this.queueA.push(newClient);
+		newClient.registerArrival( Date.now()/1000 );
+	}
+
+
 	this.processClient = function() {
 		this.updateQueueView();
+
 		if(!this.isAttending)
 		{
 			if(this.queueA.length > 0)
 			{
-				this.currentClient = this.getNewClient();
-				this.lastServiceTime = Date.now();
+				this.currentClient = this.getNextClient();
+
+				this.lastServiceTime = Date.now()/1000;
 				this.isAttending = true;
 				console.log("got client!");
 			}
 			else
 			{
 				console.log("Waiting new client...");
+				if(this.enteredInBusyTime)
+				{
+					this.totalBusyTime += Date.now()/1000 - this.busyTimeStartTime;
+					this.enteredInBusyTime = false;
+				}
 			}
 		}
 		else
 		{
-			var now = Date.now();
+			var now = Date.now()/1000;
 			var deltaTime = now - this.lastServiceTime;
 			this.lastServiceTime = now;
-			this.currentClient.serviceTime -= deltaTime / 1000;
+			this.currentClient.residualServiceTime -= deltaTime;
 
 			console.log("Working...");
 			this.updateServerView();
-			if(this.currentClient.serviceTime <= 0)
+			this.updateAverageWaitTimeInQueue();
+			if(this.currentClient.residualServiceTime <= 0)
 			{
+				var clientSystemTime = Date.now()/1000 - this.currentClient.arrivalTime;
+				var clientWaitTime = clientSystemTime - this.currentClient.serviceTime;
+				
+				this.numOfServedClients++;
+				
+				this.totalSystemTime += clientSystemTime;
+				this.totalWaitTime += clientWaitTime;
+				
+				console.log(clientSystemTime + "  " + clientWaitTime + "  " + this.numOfServedClients);
+
 				this.isAttending = false;
 			}
 		}
 	}
 
-	this.getNewClient = function() {
+	this.getNextClient = function() {
 		return this.queueA.splice(0, 1)[0];
+	}
+
+	this.updateAverageWaitTimeInQueue = function(){
+		avgSystemTimeDiv.innerHTML 		= "E[T] = " + this.totalSystemTime/this.numOfServedClients;
+		avgWaitTimeDiv.innerHTML 		= "E[W] = " + this.totalWaitTime/this.numOfServedClients;
+		avgPendingServiceDiv.innerHTML 	= "E[U] = " + this.totalPendingService/this.totalArrivals;
+		avgClientsInQueueDiv.innerHTML 	= "E[N] = " + this.totalClientsInQueue/this.totalArrivals;
+		avgBusyTimeDiv.innerHTML 		= "E[B] = " + this.totalBusyTime/this.countOfBusyTimes;
 	}
 
 	this.updateQueueView = function(){
@@ -81,7 +150,7 @@ function QueueSimulator() {
 	}
 
 	this.updateServerView = function(){		
-		serverDiv.innerHTML = "" + this.currentClient.serviceTime;
+		serverDiv.innerHTML = "" + this.currentClient.residualServiceTime;
 	}
 }
 
